09ee181277ceb571b0d34d6823b3af3688f1265f
haokaili <>
2021-05-21 20:13:12 +0000

    finish

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ICS2O-Unit5-02-html-Positive-and-Negative
+[![GitHub's Super Linter](https://github.com/haokai-li/ICS2O-Unit5-02-html-Positive-and-Negative/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/haokai-li/ICS2O-Unit5-02-html-Positive-and-Negative/actions)
+
+This website can be found at: [https://haokai-li.github.io/ICS2O-Unit5-02-html-Positive-and-Negative](https://haokai-li.github.io/ICS2O-Unit5-02-html-Positive-and-Negative)
---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: haokai
+// Created on: May 2021
+// This is the Positive-and-Negative
+
+"use strict"
+
+function enterClicked() {
+  //input
+  const number = parseInt(document.getElementById("number").value)
+
+  // process
+  if (number < 0) {
+    //output that answer is right
+    document.getElementById('answer').innerHTML = "It is negative."
+  } else {
+    //output that answer is wrong
+    document.getElementById('answer').innerHTML = "It is positive."
+  }
+}
